9a46d33006a36a2e050afeb8005a710cfea221c2
Yash-J <jahagirdaryb@gmail.com>
2024-07-29 17:21:37 -0500

    Completed Competitive Coding 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,33 @@
+// Find the Missing Number in a sorted array 
+// Time complexity = O(log(n))
+// Space complexity = O(1)
+// Reduce the search space by 1/2
+// go towards the sorted array.
 
+class Problem1 {
+
+    public static void main(String [] args) 
+    { 
+        // a = [1,3,4,5,6,7,8];
+        // find the missing integer
+        // binary search
+        int a[] = { 1, 2, 3, 4, 5, 6, 8 };
+        int low = 0;
+        int high = a.length-1;// 7
+        while(low<=high) { // low and high . check for the last number as well
+            int mid = low + (high-low)/2; // overflow condition.// index.
+            // numbers - 1, 2, 3, 4, 5, 6, 8
+            // index -   0, 1, 2, 3, 4, 5, 6
+            // what we understand is a[i]-i=1 for all other integers except where it is missing and onwards then.
+            if(a[mid]-mid>1) {// go towards the sorted ones.
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        System.out.println(low+1);
+
+
+    }
+
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,130 @@
+// Design a Min heap
+// heap basically is a tree which is insertion is left to right
+// and like the topmost element is the minimum element.
+// Insertion is bottom-up
+// Deletion is top-down
+// Time complexity = O(log(n))
+// Space complexity = O(n)
 
+class MinHeap{
+    private int[] heap;
+    private int size;
+    private int maxSize;
+
+
+    public MinHeap() {
+        heap = new int[1000];
+        heap[0] = Integer.MIN_VALUE;
+        size =0;
+        maxSize = 1000;
+    }
+
+    private int parent(int pos) {
+        return pos/2;
+    }
+
+    private int leftChild(int pos) {
+        return 2*pos;
+    }
+    
+    private int rightChild(int pos) {
+        return (2*pos)+1;
+    }
+
+    private boolean isLeaf(int pos) {
+        return pos > (size/2);
+    }
+
+    private void swap(int fpos, int lpos) {
+        int temp = heap[fpos];
+        heap[fpos] = heap[lpos];
+        heap[lpos] = temp;
+    }
+
+    private void minHeapify(int pos) {
+        if(!isLeaf(pos)) {
+            int swapPos = pos;
+
+        if(rightChild(pos)<= size) {
+            swapPos = heap[leftChild(pos)]< heap[rightChild(pos)] ? leftChild(pos) : rightChild(pos);
+        } else {
+            swapPos = leftChild(pos);
+        }
+
+        if(heap[pos]> heap[leftChild(pos)] || heap[pos] > heap[rightChild(pos)]) {
+            swap(pos, swapPos);
+            minHeapify(swapPos);
+        }
+    }
+    }
+
+    private void insert(int element) {
+        if(size>= maxSize) {
+            return;
+        }
+        heap[++size] = element;
+        int current = size;
+        while(heap[current] < heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    private int remove() {
+        int popped = heap[1];
+        heap[1] = heap[size--];
+        minHeapify(1);
+        return popped;
+    }
+
+    private void print()
+    {
+        for (int i = 1; i <= size / 2; i++) {
+ 
+            // Printing the parent and both childrens
+            System.out.print(
+                " PARENT : " + heap[i]
+                + " LEFT CHILD : " + heap[2 * i]
+                + " RIGHT CHILD :" + heap[2 * i + 1]);
+ 
+            // By here new line is required
+            System.out.println();
+        }
+    }
+
+     // Method 10
+    // Main driver method
+    public static void main(String[] arg)
+    {
+ 
+        // Display message
+        System.out.println("The Min Heap is ");
+ 
+        // Creating object of class in main() method
+        MinHeap minHeap = new MinHeap();
+ 
+        // Inserting element to minHeap
+        // using insert() method
+ 
+        // Custom input entries
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+ 
+        // Print all elements of the heap
+        minHeap.print();
+ 
+        // Removing minimum value from above heap
+        // and printing it
+        System.out.println("The Min val is "
+                           + minHeap.remove());
+    }
+
+
+}
